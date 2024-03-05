77cdfb50b1f4fc08f7a3e6e0aaacb4a859b2e4d6
Emanuele Saccomandi <emanuele.saccomandi@gmail.com>
2024-03-05 08:40:19 +0100

    fix: readded triggers

--- models/VatType.js
+++ models/VatType.js
@@ -6,6 +6,7 @@ module.exports = {
         return [
             {
                 key: `${keyPrefix}id`,
+                dynamic: 'listVatTypesTrigger.id.name',
                 label: `Vat type id - [${labelPrefix}id]`,
                 type: 'integer',
             },
