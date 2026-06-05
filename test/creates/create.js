56b8291942da257a434d9686952ff703c2c74d9e
Mauro Valota <maurovalota@fattureincloud.it>
2026-06-05 15:52:21 +0200

    chore: test

--- test/creates/create.js
+++ test/creates/create.js
@@ -1,20 +1,20 @@
-require('should');
+require("should");
 
-const zapier = require('zapier-platform-core');
+const zapier = require("zapier-platform-core");
 
-const App = require('../../index');
+const App = require("../../index");
 const appTester = zapier.createAppTester(App);
 
-describe('Create - create', () => {
+describe("Create - create", () => {
   zapier.tools.env.inject();
 
-  it('should expose at least one valid create action', async () => {
+  it("should expose at least one valid create action", async () => {
     const createActions = Object.values(App.creates || {});
 
     createActions.length.should.be.above(0);
     createActions.forEach((action) => {
-      action.should.have.property('operation');
-      action.operation.should.have.property('perform');
+      action.should.have.property("operation");
+      action.operation.should.have.property("perform");
       action.operation.perform.should.be.a.Function();
     });
   });
